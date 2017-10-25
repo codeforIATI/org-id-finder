efd80661845432cf393e8dd17e6108c6f3f6b2c0
Andy Lulham <a.lulham@gmail.com>
2017-10-25 01:51:37 +0100

    Add app

---
+++ js/js.js
@@ -0,0 +1,44 @@
+$(function() {
+  var orgSelect = $('#org-select').select2({
+    theme: "bootstrap",
+    placeholder: "E.g. Publish What You Fund",
+    minimumInputLength: 2,
+    ajax: {
+      url: 'https://api.morph.io/andylolz/org-id-finder/data.json',
+      dataType: 'jsonp',
+      data: function (params) {
+        var query = {
+          key: 'wFTSIH61nwMjLBhphd4T',
+          query: 'select * from "organisations" where name like "%' + params.term + '%" limit 5'
+        }
+        return query;
+      },
+      processResults: function (data) {
+        var results = $.map(data, function(d) {
+          return {
+            id: d.code,
+            text: d.name,
+          }
+        })
+        return {
+          results: results
+        };
+      }
+    }
+  })
+
+  orgSelect.on('select2:select', function(e) {
+    var identifier = e.params.data.id;
+    $('#org-identifier').val(identifier);
+    $('#org-identifier-group').css('visibility','visible').hide().fadeIn('slow');
+  });
+
+  orgSelect.on('select2:open', function() {
+    if ($(this).val() !== null) {
+      $(this).val(null).trigger('change');
+    }
+    $('#org-identifier-group').css('visibility','hidden');
+  })
+
+  new Clipboard('#copy-button');
+});
